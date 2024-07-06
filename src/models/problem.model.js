642a266f0a9aee908483aa4881b556457e5bcaae
SAGNIK208 <sagnik.guru20@gmail.com>
2024-07-06 07:36:03 +0530

    added code stubs in model and repository

--- src/models/problem.model.js
+++ src/models/problem.model.js
@@ -1,37 +1,55 @@
 const mongoose = require("mongoose");
 
 const problemSchema = new mongoose.Schema({
-    title : {
-        type : String,
-        required : [true,"title cannot be empty"]
-    },
-    description : {
-        type : String,
-        required : [true,"descripiton cannot be empty"]
-    },
-    difficulty: {
+  title: {
+    type: String,
+    required: [true, "title cannot be empty"],
+  },
+  description: {
+    type: String,
+    required: [true, "descripiton cannot be empty"],
+  },
+  difficulty: {
+    type: String,
+    enum: ["easy", "medium", "hard"],
+    required: [true, "Difficulty cannot be empty"],
+    default: "easy",
+  },
+  testCases: [
+    {
+      input: {
         type: String,
-        enum: ['easy', 'medium', 'hard'],
-        required: [true, 'Difficulty cannot be empty'],
-        default: 'easy'
+        required: true,
+      },
+      output: {
+        type: String,
+        required: true,
+      },
     },
-    testCases: [
-        {
-            input: {
-                type: String,
-                required: true
-            },
-            output: {
-                type: String,
-                required: true
-            }
-        }
-    ],
-    editorial: {
-        type: String
-    }
+  ],
+  codeStubs: [
+    {
+      language: {
+        type: String,
+        enum: ["CPP", "JAVA", "PYTHON"],
+        required: true,
+      },
+      startSnippet: {
+        type: String,
+      },
+      userSnippet: {
+        type: String,
+      },
+      endSnippet: {
+        type: String,
+      },
+    },
+  ],
+  editorial: {
+    type: String,
+  },
 });
 
-const Problem = mongoose.model("Problem",problemSchema);
+const Problem = mongoose.model("Problem", problemSchema);
 
 module.exports = Problem;
--- src/repositories/problem.repository.js
+++ src/repositories/problem.repository.js
@@ -1,63 +1,64 @@
-const {Problem} = require("../models");
+const { Problem } = require("../models");
 
-class ProblemRepository{
-
-    async createProblem(problemData){
-        try {
-            const problem = await Problem.create({
-                title : problemData.title,
-                description : problemData.description,
-                testCases : (problemData.testCases) ? problemData.testCases : [],
-                difficulty : problemData.difficulty,
-                editorial : problemData?.editorial
-            });
-            return problem
-        } catch (error) {
-            console.log(error);
-            throw error;
-        }
+class ProblemRepository {
+  async createProblem(problemData) {
+    try {
+      const problem = await Problem.create({
+        title: problemData.title,
+        description: problemData.description,
+        testCases: problemData.testCases ? problemData.testCases : [],
+        difficulty: problemData.difficulty,
+        codeStubs: problemData.codeStubs,
+        editorial: problemData?.editorial,
+      });
+      return problem;
+    } catch (error) {
+      console.log(error);
+      throw error;
     }
+  }
 
-    async getAllProblems() {
-        try {
-            const problems = await Problem.find({});
-            return problems;
-        } catch(error) {
-            console.log(error);
-            throw error;
-        }
+  async getAllProblems() {
+    try {
+      const problems = await Problem.find({});
+      return problems;
+    } catch (error) {
+      console.log(error);
+      throw error;
     }
+  }
 
-    async getProblemById(id){
-        try{
-            const problem = await Problem.findById(id);
-            return problem;
-        }catch(error){
-            console.log(error);
-            throw error;
-        }
+  async getProblemById(id) {
+    try {
+      const problem = await Problem.findById(id);
+      return problem;
+    } catch (error) {
+      console.log(error);
+      throw error;
     }
+  }
 
-    async deleteProblemById(id){
-        try{
-            const problem = await Problem.findByIdAndDelete(id);
-            return problem;
-        }catch(error){
-            console.log(error);
-            throw error;
-        }
+  async deleteProblemById(id) {
+    try {
+      const problem = await Problem.findByIdAndDelete(id);
+      return problem;
+    } catch (error) {
+      console.log(error);
+      throw error;
     }
+  }
 
-    async updateProblemById(id,problemData){
-        try{
-            const problem = await Problem.findByIdAndUpdate(id,problemData,{new:true});
-            return problem;
-        }catch (error){
-            console.log(error);
-            throw error;
-        }
+  async updateProblemById(id, problemData) {
+    try {
+      const problem = await Problem.findByIdAndUpdate(id, problemData, {
+        new: true,
+      });
+      return problem;
+    } catch (error) {
+      console.log(error);
+      throw error;
     }
-
+  }
 }
 
 module.exports = ProblemRepository;
